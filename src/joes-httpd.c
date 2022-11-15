98eba851b5456f0c2ec6cb184d549b55b7a1f1ef
Joe Winett <joe@winett.com>
2022-11-15 15:41:15 -0600

    parameterizationmestiphication and stuff

--- src/joes-httpd.c
+++ src/joes-httpd.c
@@ -25,20 +25,54 @@ Use a unique ID on your spam traps for total identification domination,
 like spamtrap+JHTP32d23@abighairy.com
 */
 #include <stdio.h>
+#include <stdlib.h>
 
-char *_pstrUsage = "joes-httpd Joe's HTTP Document Server Isn't Apache, Baby (JHSIAB)\n"
+char *_pstrUsage = "\n"
+    "Joe's HTTP Document Server Isn't Apache, Baby (JHSIAB)\n"
     "Copyright (c)2022 by Joe Winett @abighairyspider - GNU Affero General Public License\n"
-    "\n"
-    "Usage: %s\n"
+    "\n"    
+    "%s %s\n"
     "\n";
+
+char *_pstrCommandPath = "joes-httpd";
+char *_pstrVerb = "[help|run]";
+
+void HelpUsageError( char *ErrorString ) {
+    
+    printf( _pstrUsage, _pstrCommandPath, _pstrVerb );    
+
+    if( ErrorString && *ErrorString ) {
+        printf( "** %s\n\n", ErrorString );                
+    }
+    
+
+}
+
     
 
 int main( int argc, char *argv[] ) {
+
+    if( argc <= 0 ) {
+        char achBuff[16];
+        snprintf( achBuff, sizeof(achBuff), "bad argc is %d", argc );
+        HelpUsageError(achBuff);
+        return -1;
+    }
+
+    _pstrCommandPath = argv[0];
+
+    if( argc == 1 ) {
+        HelpUsageError("nothing to do (no command specified)");
+        return -1;
+    }
+
+    _pstrVerb = argv[1];
+
+    HelpUsageError("unknown command");
+
+
     
-
-    printf( _pstrUsage, argv[0] );
-
-
+    
 
     return 0;
 }
